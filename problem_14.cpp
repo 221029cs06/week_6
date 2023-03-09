03ddc1055d477e76f8048b5088f78dd7729f542d
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-03-09 13:02:53 +0500

    Create problem_14.cpp

---
+++ problem_14.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char arr[11] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+    for(int i=0; i<11; i++){
+        cout<<arr[i];
+    }
+    return 0;
+}
